a43a012b3a4fc30f6f1b0da203a49745772a13ee
devin-hale <devinwaynehale@gmail.com>
2023-09-26 10:56:56 -0500

    Began class instance slice

--- src/app/dashboard/classes/[classId]/page.tsx
+++ src/app/dashboard/classes/[classId]/page.tsx
@@ -3,6 +3,6 @@ import { useRouter } from "next/navigation";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 
 export default function ClassInstancePagea({ params }: { params: { classId: string } }): JSX.Element {
-	const nextRouter: AppRouterInstance = useRouter();
+	const router: AppRouterInstance = useRouter();
 	return <div>{params.classId}</div>
 }
---
+++ src/redux/slices/classInstanceSlice.ts
@@ -0,0 +1,101 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { IClass } from './classSlice';
+
+const APIDOMAIN: string = process.env.NEXT_PUBLIC_APIDOMAIN!;
+
+export type ClassInstanceState = {
+	loading: boolean;
+	message: string | null;
+	error: boolean;
+	classInfo: IClass | null;
+	students: IStudent[];
+};
+export type IStudent = {
+	_id: string;
+	first_name: string;
+	last_name: string;
+	grade_level: number;
+	gifted: boolean;
+	retained: boolean;
+	sped: boolean;
+	english_language_learner: boolean;
+	classes?: string[];
+	org: string;
+	active: string;
+};
+type getReqInfo = {
+	classId: string;
+	token: string;
+};
+
+export const getClassInfo = createAsyncThunk(
+	'classInstance/getInfo',
+	async (getReqInfo: getReqInfo) => {
+		const request = await fetch(`${APIDOMAIN}/classes/${getReqInfo.classId}`, {
+			method: 'get',
+			headers: {
+				Authorization: `Bearer ${getReqInfo.token}`,
+			},
+			mode: 'cors',
+		});
+		const data = await request.json();
+		return data;
+	}
+);
+
+export const getClassStudents = createAsyncThunk(
+	'classInstance/getStudents',
+	async (getReqInfo: getReqInfo) => {
+		const request = await fetch(`${APIDOMAIN}/students/class/${getReqInfo.classId}`, {
+			method: 'get',
+			headers: {
+				Authorization: `Bearer ${getReqInfo.token}`,
+			},
+			mode: 'cors',
+		});
+		const data = await request.json();
+		return data;
+	}
+);
+
+const initialState: ClassInstanceState = {
+	loading: false,
+	message: '',
+	error: false,
+	classInfo: null,
+	students: [],
+};
+
+export const classSlice = createSlice({
+	name: 'userClasses',
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(getClassInfo.pending, (state) => {
+				state.loading = true;
+			})
+			.addCase(getClassInfo.fulfilled, (state, action) => {
+				state.loading = false;
+				state.message = action.payload.message;
+				state.error = false;
+				state.classInfo = action.payload.classInstance;
+			})
+			.addCase(getClassInfo.rejected, (state, action) => {
+				state.loading = false;
+				state.message = 'Failed to retrieve class data.';
+				state.error = true;
+				state.classInfo = null;
+			})
+			.addCase(getClassStudents.pending, (state) => {
+				state.loading = true;
+			})
+			.addCase(getClassStudents.fulfilled, (state) => {
+				state.loading = false;
+			})
+	},
+});
+
+export const {} = classSlice.actions;
+
+export default classSlice.reducer;
