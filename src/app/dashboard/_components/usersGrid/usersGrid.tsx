d3d87ff2ade2147a6318dd722a34027b55ea1db2
devin-hale <devinwaynehale@gmail.com>
2023-10-18 23:16:06 -0500

    Created users grid.

---
+++ src/app/dashboard/_components/usersGrid/usersGrid.tsx
@@ -0,0 +1,147 @@
+'use client';
+import { RootState } from '@/redux/store/store';
+import {
+	DataGrid,
+	GridColDef,
+	GridActionsCellItem,
+	GridToolbar,
+} from '@mui/x-data-grid';
+import { useRouter } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
+import { useState, useEffect, SetStateAction } from 'react';
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from '@/app/hooks';
+import { UserState } from '@/redux/slices/user/userSlice';
+import { Menu, MenuItem, Divider } from '@mui/material';
+import {
+	MoreHorizRounded,
+	ToggleOffRounded,
+	ContactPageRounded,
+} from '@mui/icons-material';
+import { getOrgUsers } from '@/redux/slices/user/userDataSlice';
+
+export default function UsersGrid(): JSX.Element {
+	const dispatch = useAppDispatch();
+	const router: AppRouterInstance = useRouter();
+	const [actionMenuAnchor, setActionMenuAchor] = useState<HTMLElement | null>(
+		null
+	);
+	const actionMenuOpen = Boolean(actionMenuAnchor);
+	const [actionMenuUserId, setActionMenuUserId] = useState<string | null>(
+		null
+	);
+
+	const user: UserState = useSelector((state: RootState) => state.user);
+	const userDataState = useSelector(
+		(state: RootState) => state.userData.orgUsers
+	);
+
+	function handleActionMenuOpen(e: React.MouseEvent<HTMLElement>) {
+		setActionMenuAchor(e.currentTarget);
+	}
+
+	function handleActionMenuClose(): void {
+		setActionMenuAchor(null);
+		setActionMenuUserId(null);
+	}
+
+	function handleActionMenuClick(
+		e: React.MouseEvent<HTMLElement>,
+		studentId: string
+	) {
+		handleActionMenuOpen(e);
+		setActionMenuUserId(studentId);
+	}
+
+	function handleStudentInfoNavigate() {
+		router.push(`/dashboard/students/${actionMenuUserId}`);
+	}
+	useEffect(() => {
+		if (user.token && user.userInfo.org) {
+			dispatch(getOrgUsers({ token: user.token!, orgId: user.userInfo.org! }));
+		}
+	}, [user.token, user.userInfo.org]);
+
+	const userGridCols: GridColDef[] = [
+		{ field: 'last_name', headerName: 'Last name', flex: 0.25 },
+		{ field: 'first_name', headerName: 'First name', flex: 0.25 },
+		{ field: 'email', headerName: 'Email', flex: 0.5 },
+		{
+			field: 'actions',
+			type: 'actions',
+			getActions: (params: any) => [
+				<GridActionsCellItem
+					key={params.id}
+					icon={<MoreHorizRounded />}
+					label='Button'
+					onClick={(e: React.MouseEvent<HTMLElement>) =>
+						handleActionMenuClick(e, params.id)
+					}
+				/>,
+			],
+		},
+	];
+
+	const userRows = [...userDataState.users].sort((a, b) =>
+		a.last_name! < b.last_name! ? -1 : 1
+	);
+
+	return (
+		<div>
+			<Menu
+				anchorEl={actionMenuAnchor}
+				open={actionMenuOpen}
+				onClick={handleActionMenuClose}
+				onClose={handleActionMenuClose}
+				transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+				anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+			>
+				<MenuItem onClick={handleStudentInfoNavigate}>
+					<ContactPageRounded
+						sx={{ width: 20 }}
+						className='mr-2'
+					/>
+					Student Info
+				</MenuItem>
+				<Divider />
+				<MenuItem onClick={() => console.log('Hey')}>
+					<ToggleOffRounded
+						sx={{ width: 20 }}
+						className='mr-2'
+					/>
+					Unenroll Student
+				</MenuItem>
+			</Menu>
+			<DataGrid
+				sx={{
+					'&.MuiDataGrid-root .MuiDataGrid-cell:focus-within': {
+						outline: 'none !important',
+					},
+					maxWidth: '90vw',
+				}}
+				rows={userRows}
+				getRowId={(row) => row._id}
+				columns={userGridCols}
+				initialState={{
+					pagination: {
+						paginationModel: { page: 0, pageSize: 10 },
+					},
+				}}
+				pageSizeOptions={[10, 25]}
+				disableColumnMenu
+				disableRowSelectionOnClick
+				disableColumnFilter
+				disableColumnSelector
+				disableDensitySelector
+				slots={{ toolbar: GridToolbar }}
+				slotProps={{
+					toolbar: {
+						showQuickFilter: true,
+						printOptions: { disableToolbarButton: true },
+						csvOptions: { disableToolbarButton: true },
+					},
+				}}
+			/>
+		</div>
+	);
+}
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+import UsersGrid from "../_components/usersGrid/usersGrid";
+
 export default function ClassPage() : JSX.Element {
-	return <div>ClassPage</div>
+	return <div><UsersGrid /></div>
 }
